d97a1af3b8f440f8c20f5101ffa5cc4b450d3e65
arous <aroush.syed@mail.utoronto.ca>
2024-11-09 16:19:45 -0500

    added profile input boundary, inputdata code

--- src/main/java/use_case/user_profile/ProfileInputData.java
+++ src/main/java/use_case/user_profile/ProfileInputData.java
@@ -1,4 +1,21 @@
 package use_case.user_profile;
 
+/**
+ * The Input Data for the Profile Use Case.
+ */
+
 public class ProfileInputData {
+    private final String[] allergies;
+    private final String[] HealthyGoals ;
+    private final String[] DietaryRestrictions;
+
+    public ProfileInputData(String[] allergies, String[] HealthyGoals, String[] DietaryRestrictions) {
+        this.allergies = allergies;
+        this.HealthyGoals = HealthyGoals;
+        this.DietaryRestrictions = DietaryRestrictions;
+    }
+
+    String[] getAllergies() { return allergies; }
+    String[] getHealthyGoals() { return HealthyGoals; }
+    String[] getDietaryRestrictions() { return DietaryRestrictions; }
 }

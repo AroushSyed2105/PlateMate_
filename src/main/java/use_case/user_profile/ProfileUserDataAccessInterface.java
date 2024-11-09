a6131123330faffd9290b479845b275ee1dcf326
fatima <fatimamasood440@gmail.com>
2024-11-09 16:42:30 -0500

    Changed the profileoutputdat and profileuserdataaccessinterface files

--- src/main/java/use_case/user_profile/ProfileUserDataAccessInterface.java
+++ src/main/java/use_case/user_profile/ProfileUserDataAccessInterface.java
@@ -1,4 +1,23 @@
 package use_case.user_profile;
 
-public class ProfileUserDataAccessInterface {
+import entity.User;
+
+public interface ProfileUserDataAccessInterface {
+
+    /**
+     * Checks if the given username exists.
+     * @param username the username to look for
+     * @return true if a user with the given username exists; false otherwise
+     */
+    boolean existsByUsername(String username);
+
+    User getProfileByUsername(String username);
+
+    /**
+     * Saves the user.
+     * @param user the user to save
+     */
+    void saveProfile(User user);
+
+    void deleteProfile(String username);
 }

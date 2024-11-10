353df2a030173ea45bd717b6315ea43fb435f822
Melissa Xiao <melissaxiaoo@gmail.com>
2024-11-09 22:28:54 -0500

    profile interactor trial 1

--- src/main/java/use_case/user_profile/ProfileInteractor.java
+++ src/main/java/use_case/user_profile/ProfileInteractor.java
@@ -1,4 +1,38 @@
 package use_case.user_profile;
 
+import entity.User;
+import entity.UserFactory;
+
 public class ProfileInteractor {
+    private final ProfileUserDataAccessInterface userDataAccessObject;
+    private final ProfileOutputBoundary profilePresenter;
+    private final UserFactory userFactory;
+
+    public ProfileInteractor(ProfileUserDataAccessInterface profileUserDataAccessInterface,
+                            ProfileOutputBoundary profileOutputBoundary,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = profileUserDataAccessInterface;
+        this.profilePresenter = profileOutputBoundary;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(ProfileOutputData profileOutputData) {
+        if (userDataAccessObject.existsByUsername(profileOutputData.getUsername())) {
+            profilePresenter.prepareFailView("Profile already exists.");
+        }
+        else {
+
+            final User user = userFactory.create(profileOutputData.getUsername(), profileOutputData.getPassword());
+            userDataAccessObject.save(user);
+
+            final ProfileOutputData ProfileOutputData = new ProfileOutputData(user.getName(), false);
+            profilePresenter.prepareSuccessView(signupOutputData);
+        }
+    }
+
+    @Override
+    public void switchToProfileView() {
+        profilePresenter.switchToProfileView();
+    }
 }

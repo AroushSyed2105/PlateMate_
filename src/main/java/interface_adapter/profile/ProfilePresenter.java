1531274de36edf99447ce0de88be00ce2f8d6c57
Mayesha Rahaman <mayesha.rahaman@mail.utoronto.ca>
2024-11-10 13:40:05 -0500

    updated ProfilePresenter - first edit

--- src/main/java/interface_adapter/profile/ProfilePresenter.java
+++ src/main/java/interface_adapter/profile/ProfilePresenter.java
@@ -1,5 +1,49 @@
 package interface_adapter.profile;
 
-public class ProfilePresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.change_password.LoggedInState;
+import interface_adapter.change_password.LoggedInViewModel;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import use_case.user_profile.ProfileOutputBoundary;
+import use_case.user_profile.ProfileOutputData;
+
+/**
+ * The Presenter for the Login Use Case.
+ */
+public class ProfilePresenter implements ProfileOutputBoundary {
+    private final ProfileViewModel viewModel;
+    private final LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public ProfilePresenter(ProfileViewModel viewModel, ViewManagerModel viewManagerModel,
+                            LoggedInViewModel loggedInViewModel,
+                            LoginViewModel loginViewModel) {
+        this.viewModel = viewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(ProfileOutputData response) {
+        // On success, switch to the logged in view.
+
+        final LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername(response.getUsername());
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(loggedInViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        final LoginState loginState = loginViewModel.getState();
+        loginState.setLoginError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
 

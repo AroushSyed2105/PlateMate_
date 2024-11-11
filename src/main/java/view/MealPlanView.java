9c59ccdcf9df47147afb2d123f656633e92d05e8
arous <aroush.syed@mail.utoronto.ca>
2024-11-11 16:03:51 -0500

    Debugged MealPlanViewModel

--- src/main/java/view/MealPlanView.java
+++ src/main/java/view/MealPlanView.java
@@ -3,7 +3,7 @@ package view;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class MealPlanViewModel {
+public class MealPlanView {
     public static void main(String[] args) {
         // Create the frame
         JFrame frame = new JFrame("Temporary Screen");

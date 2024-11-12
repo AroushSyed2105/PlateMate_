e0bcf4ab3b00f8355d36ab75a3285cc4abe9c880
arous <aroush.syed@mail.utoronto.ca>
2024-11-11 23:10:42 -0500

    Worked on MealPlaneState

--- src/main/java/interface_adapter/meal_plan/MealPlanState.java
+++ src/main/java/interface_adapter/meal_plan/MealPlanState.java
@@ -1,4 +1,23 @@
 package interface_adapter.meal_plan;
 
+/**
+ * The state for the MealPlan View Model.
+ */
 public class MealPlanState {
+    private String[] allergies = {};
+    private String[] healthGoals = {};
+    private String[] dietaryRestrictions = {};
+
+    public String[] getAllergies() { return allergies; }
+
+    public String[] getHealthGoals() { return healthGoals; }
+
+    public String[] getDietaryRestrictions() { return dietaryRestrictions; }
+
+    public void setAllergies(String[] allergies) { this.allergies = allergies; }
+
+    public void setHealthGoals(String[] healthGoals) { this.healthGoals = healthGoals; }
+
+    public void setDietaryRestrictions(String[] dietaryRestrictions) {this.dietaryRestrictions = dietaryRestrictions; }
+
 }

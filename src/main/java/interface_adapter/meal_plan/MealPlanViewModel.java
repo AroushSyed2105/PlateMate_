66ff55103a61d5922565ffe77cde024ef36bbad9
Mayesha Rahaman <mayesha.rahaman@mail.utoronto.ca>
2024-11-12 11:12:22 -0500

    added MealPlanViewModel and its constructor

--- src/main/java/interface_adapter/meal_plan/MealPlanViewModel.java
+++ src/main/java/interface_adapter/meal_plan/MealPlanViewModel.java
@@ -3,7 +3,7 @@ package interface_adapter.meal_plan;
 import interface_adapter.ViewModel;
 import interface_adapter.profile.ProfileState;
 
-public class MealPlanViewModel extends ViewModel<ProfileState> {
+public class MealPlanViewModel extends ViewModel<MealPlanState> {
 
     public MealPlanViewModel() {
         super("Meal Plan");

53ac9b415e26f824fb76f02891e90aaecc8d190e
Mayesha Rahaman <mayesha.rahaman@mail.utoronto.ca>
2024-11-15 14:50:58 -0500

    created LoggedInOutputBoundary file

---
+++ src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
@@ -0,0 +1,15 @@
+package use_case.logged_in;
+
+import use_case.signup.SignupOutputData;
+import use_case.user_profile.ProfileOutputData;
+
+public interface LoggedInOutputBoundary {
+    void prepareSuccessView(ProfileOutputData outputData);
+
+
+    void prepareFailView(String errorMessage);
+
+
+    void switchToProfileView();
+}
+

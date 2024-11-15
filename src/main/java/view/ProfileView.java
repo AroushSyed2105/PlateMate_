268e4ca03939593adc5a8795d2174717317ff864
Mayesha Rahaman <mayesha.rahaman@mail.utoronto.ca>
2024-11-15 14:56:18 -0500

    checked out ProfileView

--- src/main/java/view/ProfileView.java
+++ src/main/java/view/ProfileView.java
@@ -1,5 +1,4 @@
 package view;
-import java.util.Arrays;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +9,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;

d83c32a7e2d2890d45a67844445354e02cdd3894
Melissa Xiao <melissa.xiao@mail.utoronto.ca>
2024-11-28 09:15:48 -0500

    Added navigation functionality between views

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -22,7 +22,7 @@ public class Main {
                                             .addChangePasswordUseCase()
                                             .addLogoutUseCase()
                                             .addProfileView()
-//                                            .addMealPlanView()
+                                            .addMealPlanView()
                                             .addProfileUseCase()
                                             .build();
 

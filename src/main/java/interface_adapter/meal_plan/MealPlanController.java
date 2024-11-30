bc1bd3119ce620cf2a4cdfb78054cab07a8b669b
arous <aroush.syed@mail.utoronto.ca>
2024-11-30 02:42:46 -0500

    I pulled all the parser changes I made in DBUserDataAccessObject, the UI changes in SignupView, GroceryView,MealplanView and LoggedInview.

--- src/main/java/interface_adapter/meal_plan/MealPlanController.java
+++
@@ -1,6 +0,0 @@
-package interface_adapter.meal_plan;
-
-import use_case.meal_plan.MealPlan
-
-public class MealPlanController {
-}
--- src/main/java/use_case/meal_plan/MealPlanInputData.java
+++
@@ -1,4 +0,0 @@
-package use_case.meal_plan;
-
-public class MealPlanInputData {
-}
